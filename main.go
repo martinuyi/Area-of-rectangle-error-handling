33955bf65050558c62f83dfd26daae360ec1364b
Martin Uyi <2019251351@ub.edu.bz>
2023-02-06 11:19:43 -0600

    adding error checking to area

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 )
 
-func area(length float64, width float64) float64 {
-	return length * width
+func area(length float64, width float64) (float64, error) {
+	// check if length is greater than 0
+	if length < 0 {
+		err := errors.New("you cannot have a negative length")
 
+		return -1, err
+	}
+
+	if width < 0 {
+		err := errors.New("you cannot have a negative length")
+
+		return -1, err
+	}
+	result := length * width
+	return result, nil
 }
 
 func main() {
 
-	result := area(5.5, 10)
+	length := 5.5
+	width := 10
+
+	result, err := area(length, float64(width))
+	//fmt.Println(result)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(result)
 
 }
